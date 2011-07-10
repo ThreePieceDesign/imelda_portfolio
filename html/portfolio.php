1759be97c37053dbf0eda3d9ca7dc183e40339e5
Imelda Guzman <imelda@threepiecedesign.com>
2011-07-10 14:19:32 -0700

    updated images for ezflightchart and added images for joyna's website

--- html/portfolio.php
+++ html/portfolio.php
@@ -4,6 +4,21 @@
 ?>		
         <h2>Portfolio</h2>
         <div id="gallery">
+         <div class="portfolio-box">
+                <p class="center">
+                	<a href="/images/ezflightchart.jpg" rel="ezflightchart" title="EzflightChart Home Page"><img src="/images/ezflightchart-tn.jpg" rel="ezflightchart" alt="EzflightChart Home Page" title="EzflightChart Home Page" /></a>
+                    <a href="/images/ezflightchart-subpage1.jpg" rel="ezflightchart" title="EzflightChart Secondary Page"></a>
+                </p>
+                <p>Website for printing company.  Acting designer and developer for the project.  Project created in June, 2011.</p>
+            </div>
+        <div class="portfolio-box">
+                <p class="center">
+                	<a href="/images/joynaheinz.jpg" rel="joyna" title="Joyna Heinz Home Page"><img src="/images/joynaheinz-tn.jpg" rel="joyna" alt="Joyna Heinz Home Page" title="Joyna Heinz Home Page" /></a>
+                    <a href="/images/joynaheinz-subpage1.jpg" rel="joyna" title="Joyna Heinz Secondary Page"></a>
+                    <a href="/images/joynaheinz-subpage2.jpg" rel="joyna" title="Joyna Heinz Secondary Page"></a>
+                </p>
+                <p>Website for local Artist, Joyna Heinz.  Acting developer for project. Project created in June 2011./p>
+            </div>
             <div class="portfolio-box">
                 <p class="center">
                 	<a href="/images/refb_index.jpg" rel="refb" title="REFB Home Page"><img src="/images/refb_index_tn.jpg" rel="refb" alt="REFB Home Page" title="REFB Home Page" /></a>
@@ -16,15 +31,7 @@
                 	<a href="/images/wine_label.jpg" rel="winelabel" title="Wine Label"><img src="/images/wine_label_tn.jpg" alt="Wine Label" /></a>
                 </p>
                 <p>Print media using Illustrator.  Class project created in Fall of 2010.</p>
-            </div>
-            <div class="portfolio-box">
-                <p class="center">
-                	<a href="/images/ezflight_index.jpg" rel="ezflight" title="Ezflight Home Page"><img src="/images/ezflight_index_tn.jpg" rel="ezflight" alt="Ezflight Home Page" title="Ezflight Home Page" /></a>
-                    <a href="/images/ezflight_subpage1.jpg" rel="ezflight" title="Ezflight Secondary Page"></a>
-                    <a href="/images/ezflight_subpage2.jpg" rel="ezflight" title="Ezflight Secondary Page"></a>
-                </p>
-                <p>Website for printing company.  Acting designer and developer for the project.  Project created on Oct, 2010.</p>
-            </div>
+            </div>           
         </div>
         <div id="contact-info">
         	<strong>For Services, Telephone or email</strong><br />
